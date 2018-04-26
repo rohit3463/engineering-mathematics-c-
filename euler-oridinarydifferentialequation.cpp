d4d0077a5d62463a7cd4781d362ca3f0ac10b00e
Rohit Sethi <31153684+rohit3463@users.noreply.github.com>
2018-04-26 14:33:16 +0530

    Add files via upload

---
+++ euler-oridinarydifferentialequation.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+ 
+using namespace std;
+
+float func(float x,float y)
+{
+	return (x+y+x*y);
+}
+
+void euler(float x0,float y,float h, float x)
+{
+	float temp = 0;
+
+	while(x0 < x)
+	{
+		temp = y;
+		y = y + h * func(x0,y);
+		x0 = xo + h;
+	}
+	cout<<"Approximate solution at x =" <<x<<" is "<<y<<endl;
+}
+
+int main()
+{
+	float x0 = 0,y0 = 1,h = 0.025,x = 0.1;
+	euler(x0,y0,h,x);
+	return 0;
+}
